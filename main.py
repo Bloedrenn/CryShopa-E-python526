25283312cc0e1e4e5e9fcde4ca4857413839cf77
Bloedrenn <bloedrenn@gmail.com>
2026-03-22 10:20:14 +0300

    E: cоздал тестовые эндпоинты - параметры пути, порядок эндпоинтов

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ async def welcome_user(first_name: str, last_name: str) -> dict:
 @app.get("/order/{order_id}")
 async def get_order(order_id: int) -> dict:
     return {"id": order_id}
+
+
+# Все фиксированные пути должны быть объявлены первыми перед динамических путями с параметрами пути
+@app.get("/user/profile")
+async def profile() -> dict:
+    return {"profile": "View profile user"}
+
+
+@app.get("/user/{user_name}")
+async def user(user_name: str) -> dict:
+    return {"user": user_name}
