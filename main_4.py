810f21be53a235bdba66bb8d269ab8785e170eb5
Bloedrenn <bloedrenn@gmail.com>
2026-04-05 12:19:53 +0300

    E: конспект - pydantic-схемы, Field

--- main_4.py
+++ main_4.py
@@ -117,3 +117,19 @@
 # # print(
 # #   user.password.get_secret_value()
 # # )
+
+
+
+
+# ========================================================================================= #
+from pydantic import BaseModel, Field
+
+
+class MessageRead(BaseModel):
+  id: int = Field(gt=0) # Число: больше 0
+  content: str = Field(min_length=1, max_length=500, pattern=r"^[a-zA-Z0-9\s!,.?]*$") # Строка: 1-500 символов, только буквы, цифры, пробельные символы и конкретные знаки
+  priority: float = Field(default=0.0, ge=0.0, le=10.0) # Число: необязательное, от 0 до 10
+
+
+message = MessageRead(id=1, content="Hello, world!", priority=5.0)
+print(message)
