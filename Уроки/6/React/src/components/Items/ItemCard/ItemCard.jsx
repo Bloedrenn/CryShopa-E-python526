a5691dbe0a1669d6c503df65094feca88039e4e3
Bloedrenn <bloedrenn@gmail.com>
2026-04-25 11:42:23 +0300

    feat(ItemCard): Добавить иконки: edit и delete.

--- Уроки/6/React/src/components/Items/ItemCard/ItemCard.jsx
+++ Уроки/6/React/src/components/Items/ItemCard/ItemCard.jsx
@@ -1,8 +1,23 @@
+import { BsTrash3Fill } from "react-icons/bs"
+import { FaEdit } from "react-icons/fa"
+
 import styles from './ItemCard.module.css'
 
 
 const ItemCard = ({ item }) => (
   <div className={styles.item}>
+    <BsTrash3Fill
+      title="Удалить"
+      className={styles.deleteIcon}
+      size={30}
+      color="red"
+    />
+    <FaEdit
+      title="Редактировать"
+      className={styles.editIcon}
+      size={35}
+      color="#000"
+    />
     <strong>{item.name}</strong>
     <div>{item.description}</div>
     <div>{item.isAvailable ? 'В наличии :)' : 'Нет в наличии :('}</div>
