ab68f0c49f3a5be4bd3045f439b599dfdd699451
Bloedrenn <bloedrenn@gmail.com>
2026-04-25 11:48:47 +0300

    feat(AddItem): Подключить компонент AddItem в App.

--- Уроки/6/React/src/App.jsx
+++ Уроки/6/React/src/App.jsx
@@ -1,5 +1,8 @@
+import AddItem from '@components/Items/Forms/AddItem'
 import Header from '@components/UI/Header'
 import ItemList from '@components/Items/ItemList'
+
+import styles from './App.module.css'
 
 
 function App() {
@@ -7,7 +10,12 @@ function App() {
     <>
       <Header />
 
-      <ItemList />
+      <main className={styles.main}>
+        <ItemList />
+      </main>
+      <aside className={styles.aside}>
+        <AddItem />
+      </aside>
     </>
   )
 }
